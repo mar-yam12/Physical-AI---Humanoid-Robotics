38b749053b5e4e2652e1220bf69c23f4904da4db
Maryam Shahid <maryamshahid772@gmail.com>
2025-11-30 03:45:45 +0500

    maryam

---
+++ physical-ai-humanoid-robotics-textbook/.docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\src\\Desktop\\Book\\physical-ai-humanoid-robotics-textbook\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\src\\Desktop\\Book\\physical-ai-humanoid-robotics-textbook\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\src\\Desktop\\Book\\physical-ai-humanoid-robotics-textbook\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\src\\Desktop\\Book\\physical-ai-humanoid-robotics-textbook\\src\\css\\custom.css"),
+];
